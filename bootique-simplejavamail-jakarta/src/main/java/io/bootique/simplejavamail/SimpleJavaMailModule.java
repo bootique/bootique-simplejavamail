420fa6f479b3fb53d9fc63f1680f88800f58256f
Andrus Adamchik <andrus@objectstyle.com>
2023-11-24 15:11:10 -0500

    Upgrading to the latest BQModuleProvider format

--- bootique-simplejavamail-jakarta/src/main/java/io/bootique/simplejavamail/SimpleJavaMailModule.java
+++ bootique-simplejavamail-jakarta/src/main/java/io/bootique/simplejavamail/SimpleJavaMailModule.java
@@ -1,28 +1,32 @@
 package io.bootique.simplejavamail;
 
-import io.bootique.BaseModule;
+import io.bootique.BQModuleProvider;
+import io.bootique.ConfigModule;
+import io.bootique.bootstrap.BuiltModule;
 import io.bootique.config.ConfigurationFactory;
+import io.bootique.di.Binder;
 import io.bootique.di.Provides;
 import io.bootique.shutdown.ShutdownManager;
 
 import javax.inject.Singleton;
-import java.lang.reflect.Type;
-import java.util.Collections;
-import java.util.Map;
 
 /**
  * @since 2.0
  */
-public class SimpleJavaMailModule extends BaseModule {
+public class SimpleJavaMailModule extends ConfigModule implements BQModuleProvider {
+
+    @Override
+    public BuiltModule buildModule() {
+        return BuiltModule.of(this).provider(this).config(configPrefix, MailersFactory.class).build();
+    }
+
+    @Override
+    public void configure(Binder binder) {
+    }
 
     @Singleton
     @Provides
     Mailers provideMailers(ConfigurationFactory configurationFactory, ShutdownManager shutdownManager) {
         return config(MailersFactory.class, configurationFactory).createMailers(shutdownManager);
-    }
-
-    @Override
-    public Map<String, Type> configs() {
-        return Collections.singletonMap(configPrefix, MailersFactory.class);
     }
 }
