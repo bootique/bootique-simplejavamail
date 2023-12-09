28d789e39b40f736e199ce335d771a8f9a93a3bc
Andrus Adamchik <andrus@objectstyle.com>
2023-12-09 08:45:09 -0500

    Upgrade SimpleJavaMail Jakarta to v8 #8

--- bootique-simplejavamail-jakarta/src/main/java/io/bootique/simplejavamail/MailerWithOverriddenRecipients.java
+++ bootique-simplejavamail-jakarta/src/main/java/io/bootique/simplejavamail/MailerWithOverriddenRecipients.java
@@ -21,15 +21,21 @@ package io.bootique.simplejavamail;
 import jakarta.mail.Address;
 import jakarta.mail.MessagingException;
 import jakarta.mail.Session;
+import jakarta.mail.Transport;
 import jakarta.mail.internet.MimeMessage;
 import org.simplejavamail.MailException;
 import org.simplejavamail.api.email.Email;
+import org.simplejavamail.api.internal.batchsupport.LifecycleDelegatingTransport;
 import org.simplejavamail.api.mailer.CustomMailer;
 import org.simplejavamail.api.mailer.config.OperationalConfig;
+import org.simplejavamail.internal.moduleloader.ModuleLoader;
+import org.simplejavamail.internal.modules.BatchModule;
+import org.simplejavamail.mailer.internal.util.TransportConnectionHelper;
 import org.simplejavamail.mailer.internal.util.TransportRunner;
 import org.slf4j.Logger;
 
 import java.util.Objects;
+import java.util.UUID;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -55,14 +61,53 @@ public class MailerWithOverriddenRecipients implements CustomMailer {
 
     @Override
     public void sendMessage(OperationalConfig operationalConfig, Session session, Email email, MimeMessage message) {
+
         try {
-            // doing the same as SendMailClosure, except override the recipients
-            TransportRunner.sendMessage(operationalConfig.getClusterKey(), session, message, overriddenRecipients);
+            // doing something similar to what SendMailClosure does, except override the recipients
+            sendMessage(operationalConfig.getClusterKey(), session, message);
         } catch (MessagingException e) {
             LOGGER.error("Failed to send email:\n{}", email.getId());
             LOGGER.trace("{}", email);
             throw new MailerWithOverriddenRecipientsException("Third party error", e);
         }
+    }
+
+    // loosely copied from SJM TransportRunner
+    private void sendMessage(UUID clusterKey, Session session, MimeMessage message) throws MessagingException {
+        TransportRunner.TransportRunnable runnable = (transport, actualSessionUsed) -> {
+            transport.sendMessage(message, overriddenRecipients);
+            LOGGER.trace("...email sent");
+        };
+
+        if (ModuleLoader.batchModuleAvailable()) {
+            sendUsingConnectionPool(ModuleLoader.loadBatchModule(), clusterKey, session, false, runnable);
+        } else {
+            try (Transport transport = session.getTransport()) {
+                TransportConnectionHelper.connectTransport(transport, session);
+                runnable.run(transport, session);
+            } finally {
+                LOGGER.trace("closing transport");
+            }
+        }
+    }
+
+    // copied from SJM TransportRunner
+    private static void sendUsingConnectionPool(
+            BatchModule batchModule,
+            UUID clusterKey,
+            Session session,
+            boolean stickySession,
+            TransportRunner.TransportRunnable runnable) throws MessagingException {
+
+        LifecycleDelegatingTransport delegatingTransport = batchModule.acquireTransport(clusterKey, session, stickySession);
+        try {
+            runnable.run(delegatingTransport.getTransport(), delegatingTransport.getSessionUsedToObtainTransport());
+        } catch (Throwable t) {
+            // always make sure claimed resources are released
+            delegatingTransport.signalTransportFailed();
+            throw t;
+        }
+        delegatingTransport.signalTransportUsed();
     }
 
     // SJM throws MailerException that is not public, so the closest things we can do is our own subclass of MailException
