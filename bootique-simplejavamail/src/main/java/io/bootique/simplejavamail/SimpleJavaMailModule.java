50d6c55b07534c97c5fad7f3710fce27ca8431e0
Andrus Adamchik <andrus@objectstyle.com>
2023-12-09 10:21:39 -0500

    refactoring - do not inherit from ConfigModule.
    It doesn't add anything valuable to the picture anymore

--- bootique-simplejavamail/src/main/java/io/bootique/simplejavamail/SimpleJavaMailModule.java
+++ bootique-simplejavamail/src/main/java/io/bootique/simplejavamail/SimpleJavaMailModule.java
@@ -1,8 +1,9 @@
 package io.bootique.simplejavamail;
 
-import io.bootique.ConfigModule;
+import io.bootique.BQModule;
 import io.bootique.ModuleCrate;
 import io.bootique.config.ConfigurationFactory;
+import io.bootique.di.Binder;
 import io.bootique.di.Provides;
 import io.bootique.shutdown.ShutdownManager;
 
@@ -13,19 +14,25 @@ import javax.inject.Singleton;
  * @deprecated in favor of the Jakarta flavor
  */
 @Deprecated(since = "3.0", forRemoval = true)
-public class SimpleJavaMailModule extends ConfigModule {
+public class SimpleJavaMailModule implements BQModule {
+
+    private static final String CONFIG_PREFIX = "simplejavamail";
 
     @Override
     public ModuleCrate crate() {
         return ModuleCrate.of(this)
                 .description("Deprecated, can be replaced with 'bootique-simplejavamail-jakarta'.")
-                .config(configPrefix, MailersFactory.class)
+                .config(CONFIG_PREFIX, MailersFactory.class)
                 .build();
+    }
+
+    @Override
+    public void configure(Binder binder) {
     }
 
     @Singleton
     @Provides
-    Mailers provideMailers(ConfigurationFactory configurationFactory, ShutdownManager shutdownManager) {
-        return config(MailersFactory.class, configurationFactory).createMailers(shutdownManager);
+    Mailers provideMailers(ConfigurationFactory configFactory, ShutdownManager shutdownManager) {
+        return configFactory.config(MailersFactory.class, CONFIG_PREFIX).createMailers(shutdownManager);
     }
 }
